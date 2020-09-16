4eff098890170d1a228bba5586672041675bb45c
rober <me@rober.dev>
2020-09-16 10:52:27 +0200

    Home page in client

---
+++ client/src/pages/index.js
@@ -0,0 +1,13 @@
+// Vendor libs
+import React from 'react';
+
+// Component definition
+const HomePage = () => {
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  );
+};
+
+export default HomePage;
