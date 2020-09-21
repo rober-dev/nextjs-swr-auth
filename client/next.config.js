41496e207ea12b32f0bac992c9b2379299355112
rober <me@rober.dev>
2020-09-21 11:04:52 +0200

    Next.js rewrites

--- client/next.config.js
+++ client/next.config.js
@@ -1,27 +1,34 @@
 module.exports = {
   distDir: 'dist',
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require('./src/generate-sitemap');
+    }
+
+    return config;
+  },
   async rewrites() {
     return [
       {
         source: '/es/marcas',
-        destination: '/es/brands',
+        destination: '/es/brands'
       },
       {
         source: '/gl/marcas',
-        destination: '/gl/brands',
+        destination: '/gl/brands'
       },
       {
         source: '/es/marcas/:slug*',
-        destination: '/es/brands/:slug*',
+        destination: '/es/brands/:slug*'
       },
       {
         source: '/gl/marcas/:slug*',
-        destination: '/gl/brands/:slug*',
-      },
+        destination: '/gl/brands/:slug*'
+      }
     ];
   },
   env: {
     API_URL: process.env.API_URL,
-    PORT: process.env.PORT,
-  },
+    PORT: process.env.PORT
+  }
 };
